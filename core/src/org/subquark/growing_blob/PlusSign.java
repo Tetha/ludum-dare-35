e88122ec50638f7d6c011609d0e8edaad6c0a988
Harald Krämer <tetha42@gmail.com>
2016-04-17 18:42:51 +0200

    added little plus renderer

---
+++ core/src/org/subquark/growing_blob/PlusSign.java
@@ -0,0 +1,35 @@
+package org.subquark.growing_blob;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class PlusSign extends Actor {
+    private final ShapeRenderer shapeRenderer;
+
+    public PlusSign() {
+        this.shapeRenderer = new ShapeRenderer();
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.end();
+        shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
+        shapeRenderer.setTransformMatrix(batch.getTransformMatrix());
+        shapeRenderer.translate(getX(), getY(), 0);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        shapeRenderer.setColor(0, 0, 0, parentAlpha);
+        shapeRenderer.rect(0, getHeight()/3, getWidth(), getHeight()/3);
+        shapeRenderer.rect(getWidth()/3, 0, getWidth()/3, getHeight());
+
+        shapeRenderer.setColor(getColor());
+        shapeRenderer.rect(0 + 1, getHeight()/3 + 1, getWidth() - 2, getHeight()/3 - 2);
+        shapeRenderer.rect(getWidth()/3 + 1, 0 + 1, getWidth()/3 - 2, getHeight() - 2);
+
+        shapeRenderer.end();
+
+        batch.begin();
+    }
+}
