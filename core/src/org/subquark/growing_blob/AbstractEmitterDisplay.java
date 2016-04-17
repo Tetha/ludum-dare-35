e7305ee8d483a864bfbc13196f7b573a5b047eba
Harald Krämer <tetha42@gmail.com>
2016-04-17 15:20:13 +0200

    Speed up things *again*

--- core/src/org/subquark/growing_blob/AbstractEmitterDisplay.java
+++ core/src/org/subquark/growing_blob/AbstractEmitterDisplay.java
@@ -4,6 +4,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public abstract class AbstractEmitterDisplay extends Actor {
     protected float travelTime(float distance) {
-        return Math.abs(1 * distance);
+        return Math.abs(0.3f * distance);
     }
 }
